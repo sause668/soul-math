c8a4dda94615bc436ccd9e6c0be55be8bf4da89d
sause668 <soulsitescoding@zohomail.com>
2023-04-23 21:51:46 -0400

    testing

--- src/components/QuizStart.js
+++ src/components/QuizStart.js
@@ -16,9 +16,16 @@ export default function QuizStart({quiz, pageState, setPageState, unitTitle}) {
                 alignItems: 'center',
                 width: '100%',
         }}>
-            <Typography align='center' variant='h1' m={0} >{unitTitle}k</Typography>
-            <Typography align='center' variant='h2' m={4} >{quiz.title}a</Typography>
-            <Typography align='center' variant='h3' m={2} px={0} ><b>Instructions: </b>{quiz.dis}m</Typography>
+            <Typography align='center' variant='h1' m={0} >{unitTitle}</Typography>
+            <Typography align='center' variant='h2' m={4} >{quiz.title}</Typography>
+            
+            <Button 
+                variant='contained'
+                onClick={()=>setPageState(0)}
+                sx={{m: 3}}
+            >
+                <Typography align='center' variant='h3' m={2} px={0} ><b>Instructions: </b>{quiz.dis}</Typography>
+            </Button>
             <Button 
                 variant='contained'
                 onClick={()=>setPageState(0)}
